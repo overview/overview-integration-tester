7252fe5fe87b20d57cd00109e0c1b77f3697cf4b
Adam Hooper <adam@adamhooper.com>
2018-01-09 13:22:52 -0500

    Add create_document_set_from_csv

--- skeleton/helpers/session_helpers/document_set_helper.rb
+++ skeleton/helpers/session_helpers/document_set_helper.rb
@@ -26,6 +26,23 @@ module SessionHelpers
       assert_no_selector('.popover', wait: WAIT_FAST)
     end
 
+    def create_document_set_from_csv(path)
+      path = Pathname.new(path).expand_path # absolute path
+
+      visit('/')
+      click_on('Import from a CSV file', wait: WAIT_LOAD)
+      attach_file('csv-upload-file', path, wait: WAIT_LOAD)
+      click_button('Upload', match: :first, wait: WAIT_LOAD) # wait for upload to complete and JS to run
+
+      assert_selector('body.document-set-show', wait: WAIT_SLOW) # wait for import to complete
+      assert_selector('#document-list:not(.loading) li.document', wait: WAIT_LOAD) # wait for document list to load
+      # There are no plugins, so we don't need to wait for them
+
+      # Hide the Tour
+      click_link('Don’t show any more tips', wait: WAIT_FAST)
+      assert_no_selector('.popover', wait: WAIT_FAST)
+    end
+
     def create_custom_view(options)
       raise ArgumentError.new('missing options[:name]') if !options[:name]
       raise ArgumentError.new('missing options[:url]') if !options[:url]
@@ -33,17 +50,24 @@ module SessionHelpers
       click_on('Custom…', wait: WAIT_FAST)
       # Wait for focus: that's when the dialog is open
       wait_for_javascript_to_return_true('document.querySelector("#new-view-dialog-title") === document.activeElement', wait: WAIT_FAST)
-      # Fill in App URL and _then_ Name. That's because Overview has a bit of an
-      # key URL-checking feature that only checks on blur. Filling in the name
-      # _second_ means we trigger the blur.
-      fill_in('App URL', with: options[:url])
       fill_in('Name', with: options[:name])
-      click_on('use it anyway') if options[:url] =~ /^http:/ # dismiss HTTPS warning
-      assert_selector('#new-view-dialog div.ok', text: 'This URL is valid', visible: true, wait: WAIT_LOAD)
+      fill_in('App URL', with: options[:url])
+      fill_in('Overview’s URL from App server', with: OVERVIEW_URL)
+      if options[:url] =~ /^http:/
+        # dismiss HTTPS warning
+        click_on('Create visualization')
+        click_on('use it anyway', wait: WAIT_FAST)
+      end
       click_on('Create visualization')
-      # The test should wait for the plugin to load. We won't do that, because
-      # we don't know whether the plugin will create a modal dialog. (If it does,
-      # we won't be able to check for elements appearing underneath it.)
+
+      # Wait for dialog to disappear.
+      assert_no_selector('#new-view-dialog', wait: WAIT_LOAD)
+
+      # Wait for new view to appear
+      assert_selector('li.view span.title', text: options[:name], wait: WAIT_FAST)
+      # And wait for it to begin loading. Without specific plugin knowledge, we
+      # can't tell when it _ends_ loading.
+      assert_selector('iframe#view-app-iframe', wait: WAIT_FAST)
     end
 
     def delete_current_view
