bdcb8e50eaaf6d8cbd10aa4a7cefd94aaa6d9b89
Adam Hooper <adam@adamhooper.com>
2020-04-08 17:07:40 -0400

    Upgrade deps. Lots of deps.

--- test/test.sh
+++ test/test.sh
@@ -7,3 +7,4 @@ cp -av "$DIR"/../skeleton "$DIR"/framework
 cp -av "$DIR"/files-atop-framework/* "$DIR"/framework/
 sed -ie 's/^WAIT_FOR_URLS=.*/WAIT_FOR_URLS="http:\/\/overview-web"/' "$DIR"/framework/config
 "$DIR"/framework/run-in-docker-compose
+#"$DIR"/framework/run-browser ./all-tests
